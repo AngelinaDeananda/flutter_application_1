d086120f3849939f42508caef61501b390bf1b17
angelina <angelinaaharahappp@gmail.com>
2025-11-26 13:18:29 +0700

    Tugas7

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,44 @@
 import 'package:flutter/material.dart';
-import 'latihan_state_management.dart';
-import 'myxl_simple_ui.dart';
+import 'package:flutter_application_1/pages/cubit/change_color_cubit.dart';
+import 'package:flutter_application_1/pages/cubit/counter_cubit.dart';
+import 'package:flutter_application_1/pages/cubit/favorite_count_cubit.dart';
+import 'package:flutter_application_1/pages/cubit/items_cubit.dart';
+import 'package:flutter_application_1/pages/cubit/visibility_cubit.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+// import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(CounterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-    //  home: MyXLSimpleUI(), // ← memanggil UI XL
-     home: LatihanStateManagement(), // ← panggil halaman 
-    );
+    return MultiBlocProvider(
+  providers: [
+    BlocProvider(
+      create: (context) => CounterCubit(),
+    ),
+    BlocProvider(
+      create: (context) => ChangeColorCubit(),
+    ),
+    BlocProvider(
+      create: (context) => VisibilityCubit(),
+    ),
+    BlocProvider(
+      create: (context) => FavoriteCountCubit(),
+    ),
+    BlocProvider(
+      create: (context) => ItemsCubit(),
+    ),
+  ],
+  child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Loginpage(),
+  ),
+);
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // LIST DATA
-  List<String> notes = [];
-
-  // CONTROLLER INPUT
-  final TextEditingController noteController = TextEditingController();
-
-  // TAMBAH ITEM
-  void addNote() {
-    if (noteController.text.isEmpty) return;
-
-    setState(() {
-      notes.add(noteController.text);
-      noteController.clear();
-    });
-  }
-
-  // HAPUS ITEM
-  void deleteNote(int index) {
-    setState(() {
-      notes.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            // INPUT + TOMBOL TAMBAH
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: noteController,
-                    decoration: InputDecoration(
-                      hintText: "Masukkan catatan",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: addNote,
-                  child: const Text("Tambah"),
-                )
-              ],
-            ),
-
-            const SizedBox(height: 20),
-
-            // LIST
-            Expanded(
-              child: notes.isEmpty
-                  ? const Center(child: Text("Belum ada catatan"))
-                  : ListView.builder(
-                      itemCount: notes.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text(notes[index]),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.delete, color: Colors.red),
-                              onPressed: () => deleteNote(index),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
